0dc6e6d5621ae44333b9b3928f942b4b9e6779f4
agigleux <gigleux_alexandre@yahoo.fr>
2016-12-02 14:27:14 -0500

    allows RPG plugin to compile

--- travis.sh
+++ travis.sh
@@ -15,8 +15,8 @@ cd php-custom-rules
 mvn $MAVEN_CMD
 cd ..
 
-#cd rpg-custom-rules
-#curl -k https://sonarsource.bintray.com/CommercialDistribution/sonar-rpg-plugin/sonar-rpg-plugin-2.0.0.496.jar -o sonar-rpg-plugin-2.0.0.496.jar --silent
-#mv sonar-rpg-plugin-2.0.0.496.jar ./lib/sonar-rpg-plugin-2.0.0.496.jar 
-#mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
-#cd ..
+cd rpg-custom-rules
+wget https://sonarsource.bintray.com/CommercialDistribution/sonar-rpg-plugin/sonar-rpg-plugin-2.0.0.496.jar
+mv sonar-rpg-plugin-2.0.0.496.jar ./lib/sonar-rpg-plugin-2.0.0.496.jar 
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
+cd ..
