6020ca155d72200a75443a7e0d090e4d71984903
agigleux <gigleux_alexandre@yahoo.fr>
2016-12-02 14:32:41 -0500

    use the same mvn command for RPG than others

--- travis.sh
+++ travis.sh
@@ -18,5 +18,5 @@ cd ..
 cd rpg-custom-rules
 wget https://sonarsource.bintray.com/CommercialDistribution/sonar-rpg-plugin/sonar-rpg-plugin-2.0.0.496.jar
 mv sonar-rpg-plugin-2.0.0.496.jar ./lib/sonar-rpg-plugin-2.0.0.496.jar 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
+mvn $MAVEN_CMD
 cd ..
