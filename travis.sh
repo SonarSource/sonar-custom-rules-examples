a533f5857a8e181dbea02b8487caac8071c938a9
agigleux <gigleux_alexandre@yahoo.fr>
2017-05-09 08:19:40 +0200

    Take into account return code of Maven calls

--- travis.sh
+++ travis.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-MAVEN_CMD="clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=${SONARQUBE_TOKEN}"
-echo $MAVEN_CMD
+RunMaven() {
+  typeset MAVEN_CMD="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=${SONARQUBE_TOKEN}"
+  typeset RETURN_CODE
+
+  echo $MAVEN_CMD
+  eval $MAVEN_CMD
+  RETURN_CODE=$?
+  if [ $RETURN_CODE != 0 ]; then
+    printf "ERROR: [%d] when executing command: '$MAVEN_CMD'" $RETURN_CODE
+    exit $RETURN_CODE
+  fi
+}
 
 cd java-custom-rules
-mvn $MAVEN_CMD
+RunMaven
 cd ..
 
 cd javascript-custom-rules
-mvn $MAVEN_CMD
+RunMaven
 cd ..
 
 cd php-custom-rules	
-mvn $MAVEN_CMD
+RunMaven
 cd ..
 
 cd rpg-custom-rules
 wget https://sonarsource.bintray.com/CommercialDistribution/sonar-rpg-plugin/sonar-rpg-plugin-2.0.0.496.jar
 mv sonar-rpg-plugin-2.0.0.496.jar ./lib/sonar-rpg-plugin-2.0.0.496.jar 
-mvn $MAVEN_CMD
+RunMaven
 cd ..
 
 cd cobol-custom-rules
-wget https://sonarsource.bintray.com/CommercialDistribution/sonar-cobol-plugin/sonar-cobol-plugin-3.3.1.1636.jar
-mv sonar-cobol-plugin-3.3.1.1636.jar ./lib/sonar-cobol-plugin-3.3.1.1636.jar
-mvn $MAVEN_CMD
+wget https://sonarsource.bintray.com/CommercialDistribution/sonar-cobol-plugin/sonar-cobol-plugin-3.4.0.1932.jar
+mv sonar-cobol-plugin-3.4.0.1932.jar ./lib/sonar-cobol-plugin-3.4.0.1932.jar
+RunMaven
 cd ..
