dd8b677e6ca22845b3671efc9084cbbbdc3495e4
Michael Gumowski <michael.gumowski@sonarsource.com>
2020-03-12 16:08:33 +0100

    Setup sonar.organization

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RunMaven() {
-  typeset MAVEN_CMD="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=${SONARQUBE_TOKEN}"
+  typeset MAVEN_CMD="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=${SONARQUBE_TOKEN} -Dsonar.organization=sonarsource"
   typeset RETURN_CODE
 
   echo $MAVEN_CMD
